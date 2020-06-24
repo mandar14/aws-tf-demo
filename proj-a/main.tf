e81af2f0d013a3e41492d77a71de1f8631ad951f
Ubuntu <ubuntu@ip-172-31-16-181.eu-central-1.compute.internal>
2020-06-24 07:21:36 +0000

    web sg updated for public access

--- proj-a/main.tf
+++ proj-a/main.tf
@@ -38,7 +38,7 @@ resource "aws_security_group" "web" {
     from_port = 80
     to_port   = 80
     protocol  = "tcp"
-    cidr_blocks = ["10.0.0.0/24"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port = 22
